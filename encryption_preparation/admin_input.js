1431c53ca796e09b77a5c48c90948d9f25bf4bf9
charli <charlyoleg@fabfolk.com>
2019-05-09 19:52:36 +0200

    modifying the content to be encrypted

--- encryption_preparation/admin_input.js
+++ encryption_preparation/admin_input.js
@@ -16,10 +16,10 @@ exports.key_hash_salt = "blablasalt";
 
 // html to encrypt
 // the content differ between users as they may get links to different locations
-var index_jim = "hi jim<br><a href=\"hidden/a5368xjlspsawjx61xhaux5/index.html\">hidden page</a><br><a href=\"next.html\">encrypted page</a>";
-var index_jack = "hi jack<br><a href=\"hidden/h6yz6d4lsg7knctyre96yo6/index.html\">hidden page</a><br><a href=\"next.html\">encrypted page</a>";
-var next_jim = "Hi Jim! This is an new encrypte page.";
-var next_jack = "Hi jack! This is an new encrypte page. You might see something different from jack.<br><a href=\"hidden/h6yz6d4lsg7knctyre96yo6/index.html\">one linke</a>";
+var index_jim = "hi jim<br><a href=\"hidden/a5368xjlspsawjx61xhaux5/index.html\">hidden page</a><br><a href=\"next.html\">encrypted page</a><br>Updated in 2019";
+var index_jack = "hi jack<br><a href=\"hidden/h6yz6d4lsg7knctyre96yo6/index.html\">hidden page</a><br><a href=\"next.html\">encrypted page</a><br>Updated in 2019";
+var next_jim = "Hi Jim! This is an new encrypte page.<br>Updated in 2019";
+var next_jack = "Hi jack! This is an new encrypte page. You might see something different from jack.<br><a href=\"hidden/h6yz6d4lsg7knctyre96yo6/index.html\">one linke</a><br>Updated in 2019";
 
 exports.page_name = ["index", "next"];
 exports.page_content = [index_jim, index_jack, next_jim, next_jack];
